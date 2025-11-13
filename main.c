8a03637f56047fdcbe6bede86bc90e97630a7c62
sl1pknot@abv.bg <sl1pknot@abv.bg>
2025-11-13 13:14:17 +0200

    Initial commit: cpu monitor working from a system monitor project

---
+++ main.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include<unistd.h>
+
+#define token_number 11
+#define token_size 10
+
+unsigned long* read_cpu_snapshot(void);
+
+int main(void) {
+
+    unsigned long* cpu_snapshot = read_cpu_snapshot();
+    
+    unsigned long stored_cpu_snapshot[100] = {0};
+
+    for (int i = 0; cpu_snapshot[i] != '\0'; i++) {
+        stored_cpu_snapshot[i] = cpu_snapshot[i];
+    }
+    sleep(1);
+    cpu_snapshot = read_cpu_snapshot();
+
+    unsigned long total_active;
+    unsigned long total_active_curr;
+    unsigned long total_active_prev;
+    unsigned long total_idle;
+    for (int i = 0; i <= 3; i++) {
+        // total_active_curr += cpu_snapshot[i];
+        // total_active_prev += stored_cpu_snapshot[i];
+        total_active += (cpu_snapshot[i] - stored_cpu_snapshot[i]);
+    }
+    total_idle = (cpu_snapshot[3] - stored_cpu_snapshot[3]);
+
+    float cpu_usage = (((double)total_active - (double)total_idle)/(double)total_active)*100;
+    printf("CPU usage is: %.2f%%\n", cpu_usage);
+
+    return 0;
+}
+
+unsigned long* read_cpu_snapshot(void) {
+    FILE *fptr;
+    fptr = fopen("/proc/stat", "r");
+    size_t buff_size = 128;    
+    char buff[buff_size];
+    fgets(buff, buff_size, fptr);
+    buff[strcspn(buff, "\n")] = '\0';
+    fclose(fptr);
+
+    // #define token_number 11
+    // #define token_size 10
+    // const size_t token_number = 11;
+    // const size_t token_size = 10;
+
+    char token[token_number][token_size];
+    int n = 0;
+    int m = 0;
+    for (int i = 5; buff[i] != '\0'; i++) {
+        if (buff[i] == ' ') {
+            i++;
+            n++;
+            m = 0;
+        }
+        token[n][m++] = buff[i];
+    }
+
+    static unsigned long token_int[token_number];
+
+    for (int i = 0; i <= n; i++) {
+        token_int[i] = strtoul(token[i], NULL, 10);
+    }
+
+    return token_int;
+}
